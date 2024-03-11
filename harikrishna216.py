35442de8d1186b36b71aad22f7a93f868d25de49
durnuthalahari <162983116+durnuthalahari@users.noreply.github.com>
2024-03-11 16:04:13 +0530

    Add files via upload

---
+++ harikrishna216.py
@@ -0,0 +1,125 @@
+'''
+print("hello world")
+'''
+print("hello world")
+
+print("hello world")
+
+'''
+1.connecting is used for a documentation in a python file
+2.when the line is connected,that line will not be considered as part of the code
+3.when the program is executed, connected line will not be in the past of execution
+---->
+single line connecting done using # at the begining of the line
+eg:
+    #print("hello 1")
+
+print("hello world")
+
+eg:
+    '''
+print ("hello world")
+print("hello world")
+'''
+    print("hello world")
+'''
+
+
+import this
+a = 8
+
+#bargained characters
+
+controls and data structures-ABC launguage
+Stings and its formats-from C
+Regular expressins-pearl
+exception handliong-module 3
+magic methods from-fortron
+
+#what kind of launguage is python
+
+python is
+
+>object oriented programming launguage
+>dynamic typed launguage
+>scripting launguage
+>interpretsd launguage
+>compiled launguage
+>funtional programming launguage
+
+#flavours of python
+
+>Cpython-Written in C,Most common implementation of python
+>Jython-java implementation of python
+>IronPython-implementation of the python programming launguage which is tightly integrated with NET
+>Brython-Browser Python,runs in browsers
+>RubyPython-Bridge between python Ruby and interpreters
+>Micro Python=Runs in Micro Controllers
+
+#---> print(object,sep,end,file,flush)
+#print has 5 parameters
+
+#object --->the value to be printed is object
+#sep---> seperator
+#EG
+#a = 8
+#b = 9
+#c = 89
+# print(a,b,c, sep='$')
+
+#end--->which is used to addthe string at the end of the value,
+# # It will not keep the command to stay in same line
+#EG:
+#print(str(a),b,end='%%')
+#print(str(c))
+
+# ! ----> variables
+# ? variables is a name which is used to identify the value of variable
+# ? which is stored in a specific memory location
+
+# ! rules to define variables
+1.)variable ahve to start with only alphabets, underscores
+2.)the char following in the first char can be alphabets, numbers, underscores
+3.)no space are allowed
+4.)no special character like %$%^&()
+5.)case sensitive
+6.)keywords are not allowed as variables
+
+# ! ways to define variables
+
+a=8
+b=9
+print(a,b)
+
+a=7,b=9 #error
+print(a,b)
+
+a,b=7,9
+print(a)
+print(b)
+
+a=7
+b=7
+
+a = b = 7
+print(a,b)
+
+a=8,7,6.89,"hello"
+print(a)
+print(type(a))
+
+a,b=78
+print(a,b)
+
+
+
+
+
+
+
+
+
+
+
+b = 6
+
